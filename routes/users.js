e9f73d17503c0cf0a32eafbef06baeda32a42bf4
Diogo <diogopaurelio@gmail.com>
2015-06-25 10:25:16 +0200

    added login/signup forms & routes

--- routes/users.js
+++ routes/users.js
@@ -4,6 +4,18 @@ var router = express.Router();
 /* GET users listing. */
 router.get('/', function(req, res, next) {
   res.send('respond with a resource');
+
 });
+
+// GET users/register
+router.get('/register', function(req, res, next) {
+	res.render('register', { //load register view
+		'title' : 'Register' //pass in parameter
+	})
+})
+
+router.get('/login', function(req, res, next) {
+	res.render('login', { 'title': 'Login'})
+})
 
 module.exports = router;
