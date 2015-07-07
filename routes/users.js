ba2e8ff8d027c39ad65967dc4d9a596838d8d643
Diogo <diogopaurelio@gmail.com>
2015-07-07 10:37:35 +0200

    Added validation to user registration

--- routes/users.js
+++ routes/users.js
@@ -18,4 +18,71 @@ router.get('/login', function(req, res, next) {
 	res.render('login', { 'title': 'Login'})
 })
 
+//POST users/register
+router.post('/register', function(req, res, next) {
+  //retrieve form values
+  var fname = req.body.fname
+  var lname = req.body.lname
+  var email = req.body.email
+  var password = req.body.password
+  var confirmpassword = req.body.confirmpassword
+
+  if( req.files.profileimage) {
+    console.log('Uploading file...')
+    //file info
+    var profileImageOriginalName = req.profileimage.originalname
+    var profileImageOriginalName = req.profileimage.name
+    var profileImageOriginalName = req.profileimage.mimetype
+    var profileImageOriginalName = req.profileimage.path
+    var profileImageOriginalName = req.profileimage.extension
+    var profileImageOriginalName = req.profileimage.size
+
+  } else {
+    //if no pic is uploaded
+    var progileImageName = 'noImage.png'
+  }
+
+  //Form validation
+  req.checkBody('fname', 'First Name field is required').notEmpty()
+  req.checkBody('lname', 'Last Name field is required').notEmpty()
+  req.checkBody('email', 'Email not valid').isEmail()
+  req.checkBody('password', 'Password field is required').notEmpty()
+  req.checkBody('confirmpassword', 'Confirm password needs to be the same').equals(req.body.password)
+
+  // Check for errors
+  var errors = req.validationErrors()
+  console.log(errors)
+  if(errors) {
+    console.log(errors)
+    res.render('register', {
+      errors: errors,
+      fname: fname,
+      lname: lname,
+      email: email,
+      password: password,
+      confirmpassword: confirmpassword
+    })
+  } else {
+    var newUser = new User({
+      fname: fname,
+      lname: lname,
+      email: email,
+      password: password,
+      profileimage: progileImageName
+    })
+    // Create User
+    User.createUser(newUser, function(err, user) {
+      if(err) throw err
+      console.log(user)
+    })
+    req.flash('success', 'You are now registered and may log in')
+    res.location('/')
+    res.redirect('/')
+  }
+
+
+})
+
+
+
 module.exports = router;
