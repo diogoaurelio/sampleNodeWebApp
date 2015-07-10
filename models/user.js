8bea0b680a3dbf7cb36f85d2b3b1b5cdc037b52b
Diogo <diogopaurelio@gmail.com>
2015-07-10 13:37:39 +0200

    Added passport and SUCCESSFULLY changed username to email on Auth strategy

--- models/user.js
+++ models/user.js
@@ -1,5 +1,6 @@
 var mongoose = require('mongoose'),
-    db = mongoose.connection;
+    db = mongoose.connection,
+    bcrypt = require('bcrypt');
 
 mongoose.connect('mongodb://localhost/nodeauth')
 //User schema
@@ -9,6 +10,9 @@ var UserSchema = mongoose.Schema({
     index: true
   },
   lname: {
+    type: String, required: true, bcrypt: true
+  },
+  email: {
     type: String
   },
   password: {
@@ -24,6 +28,30 @@ var UserSchema = mongoose.Schema({
 
 var User = module.exports = mongoose.model('User', UserSchema)
 
+module.exports.getUserByEmail = function(email, callback) {
+  var query = { email: email }
+  User.findOne(query, callback)
+  console.log(email)
+}
+
+module.exports.getUserById = function(id, callback) {
+  User.findOne(id, callback)
+}
+
+module.exports.comparePassword = function(candidatePassword, hash, callback) {
+  bcrypt.compare(candidatePassword, hash, function(err, isMatch) {
+    if (err) return callback(err)
+    callback(null, isMatch)
+  })
+}
+
 module.exports.createUser = function(newUser, callback) {
-  newUser.save(callback)
+  bcrypt.hash(newUser.password, 10, function(err, hash) {
+    if(err) throw err
+    //Intercept and change
+    newUser.password = hash;
+    //Create user
+    newUser.save(callback)
+  })
+
 }
--- routes/users.js
+++ routes/users.js
@@ -1,90 +1,136 @@
 var express = require('express');
 var router = express.Router();
+var passport = require('passport');
+var LocalStrategy = require('passport-local').Strategy;
+var User = require('../models/user');
+var flash = require('connect-flash');
 
-var User = require('../models/user')
-
+//var port = process.env.port || 8080;
 /* GET users listing. */
 router.get('/', function(req, res, next) {
   res.send('respond with a resource');
-
 });
 
-// GET users/register
 router.get('/register', function(req, res, next) {
-	res.render('register', { //load register view
-		'title' : 'Register' //pass in parameter
-	})
-})
+  res.render('register',{
+  	'title': 'Register'
+  });
+});
 
 router.get('/login', function(req, res, next) {
-	res.render('login', { 'title': 'Login'})
-})
+  res.render('login',{
+  	'title': 'Login'
+  });
+});
 
-//POST users/register
-router.post('/register', function(req, res, next) {
-  //retrieve form values
-  var fname = req.body.fname
-  var lname = req.body.lname
-  var email = req.body.email
-  var password = req.body.password
-  var confirmpassword = req.body.confirmpassword
+router.post('/register',function(req, res, next){
+	// Get Form Values
+	var name = req.body.name;
+	var email = req.body.email;
+	var username = req.body.username;
+	var password = req.body.password;
+	var password2 = req.body.password2;
 
-  if( req.files.profileimage) {
-    console.log('Uploading file...')
-    //file info
-    var profileImageOriginalName = req.profileimage.originalname
-    var profileImageOriginalName = req.profileimage.name
-    var profileImageOriginalName = req.profileimage.mimetype
-    var profileImageOriginalName = req.profileimage.path
-    var profileImageOriginalName = req.profileimage.extension
-    var profileImageOriginalName = req.profileimage.size
+	// Check for Image Field
+	if(req.files.profileimage){
+		console.log('Uploading File...');
 
-  } else {
-    //if no pic is uploaded
-    var progileImageName = 'noImage.png'
-  }
+		// File Info
+		var profileImageOriginalName 	= req.files.profileimage.originalname;
+		var profileImageName 			= req.files.profileimage.name;
+		var profileImageMime 			= req.files.profileimage.mimetype;
+		var profileImagePath 			= req.files.profileimage.path;
+		var profileImageExt 			= req.files.profileimage.extension;
+		var profileImageSize 			= req.files.profileimage.size;
+	} else {
+		// Set a Default Image
+		var profileImageName = 'noimage.png';
+	}
 
-  //Form validation
-  req.checkBody('fname', 'First Name field is required').notEmpty()
-  req.checkBody('lname', 'Last Name field is required').notEmpty()
-  req.checkBody('email', 'Email not valid').isEmail()
-  req.checkBody('password', 'Password field is required').notEmpty()
-  req.checkBody('confirmpassword', 'Confirm password needs to be the same').equals(req.body.password)
+	// Form Validation
+	req.checkBody('name','Name field is required').notEmpty();
+	req.checkBody('email','Email field is required').notEmpty();
+	req.checkBody('email','Email not valid').isEmail();
+	req.checkBody('username','Username field is required').notEmpty();
+	req.checkBody('password','Password field is required').notEmpty();
+	req.checkBody('password2','Passwords do not match').equals(req.body.password);
 
-  // Check for errors
-  var errors = req.validationErrors()
-  console.log(errors)
-  if(errors) {
-    console.log(errors)
-    res.render('register', {
-      errors: errors,
-      fname: fname,
-      lname: lname,
-      email: email,
-      password: password,
-      confirmpassword: confirmpassword
-    })
-  } else {
-    var newUser = new User({
-      fname: fname,
-      lname: lname,
-      email: email,
-      password: password,
-      profileimage: progileImageName
-    })
-    // Create User
-    User.createUser(newUser, function(err, user) {
-      if(err) throw err
-      console.log(user)
-    })
-    req.flash('success', 'You are now registered and may log in')
-    res.location('/')
-    res.redirect('/')
-  }
+	// Check for errors
+	var errors = req.validationErrors();
 
+	if(errors){
+		res.render('register',{
+			errors: errors,
+			name: name,
+			email: email,
+			username: username,
+			password: password,
+			password2: password2
+		});
+	} else {
+		var newUser = new User({
+			name: name,
+			email: email,
+			username: username,
+			password: password,
+			profileimage: profileImageName
+		});
 
-})
+			// Create User
+	User.createUser(newUser, function(err, user){
+		if (err) throw err;
+		console.log(user);
+	});
 
 
+		// Success Message
+		req.flash('success','You are now registered and may log in');
+
+		res.location('/');
+		res.redirect('/');
+	}
+});
+
+passport.serializeUser(function(user, done) {
+  done(null, user.id);
+});
+
+passport.deserializeUser(function(id, done) {
+  User.getUserById(id, function(err, user) {
+    done(err, user);
+  });
+});
+
+//http://www.scotchmedia.com/tutorials/express/authentication/2/04
+passport.use(new LocalStrategy(
+  { usernameField: 'email' }, //have to specify this in passport if not Auth with username!!
+	function(email, password, done){
+		User.getUserByEmail(email, function(err, user){
+			if(err) throw err;
+			if(!user){
+				console.log('Unknown User');
+				return done(null, false,{message: 'Unknown User'});
+			}
+      //candidatePassword, user.password in DB, isMatch
+			User.comparePassword(password, user.password, function(err, isMatch){
+				if(err) throw err;
+				if(isMatch){
+					return done(null, user);
+				} else {
+					console.log('Invalid Password');
+					return done(null, false, {message:'Invalid Password'});
+				}
+			});
+		});
+	}
+));
+
+router.post('/login', passport.authenticate('local',{ successRedirect: '/', successFlash: 'Successfully logged in', failureRedirect:'/users/login', failureFlash:'Invalid username or password'}), function(req, res){
+  // If this function gets called, authentication was successful.
+    // `req.user` contains the authenticated user.
+  console.log('Authentication Successful');
+	req.flash('success', 'You are logged in');
+	res.redirect('/');
+});
 
 module.exports = router;
